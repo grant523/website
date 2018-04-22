1b5351e638a15a1c0c68e55f9162d1b8e62974bb
alexburov <alex.burov@outlook.com>
2018-04-21 20:47:10 -0700

    Dashboard from web2py

--- seadssite/views.py
+++ seadssite/views.py
@@ -1,21 +1,13 @@
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotAllowed
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.views.generic import View
 
+
+from seadssite.forms import UserForm
 from seadssite.models import Device
-
-import google.auth.transport.requests
-import google.oauth2.id_token
-
-HTTP_REQUEST = google.auth.transport.requests.Request()
-
-import firebase_admin
-from firebase_admin import credentials, db
-
-cred = credentials.Certificate('./seads-8023c-firebase-adminsdk-ejeko-63518da322.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://seads-8023c.firebaseio.com/'
-})
 
 
 class IndexView(TemplateView):
@@ -24,128 +16,212 @@ class IndexView(TemplateView):
     """
     template_name = 'index.html'
 
+class DashboardTest(TemplateView):
+    """
+    load main page as "index"
+    """
+    template_name = 'dashboard_test.html'
+
+
+class RegisterView(View):
+    """
+    registration page controller
+    sends a user to the registration page
+    """
+    form_class = UserForm
+    template_name = 'registration/register.html'
+
     def get(self, request):
-        authenticated = False
-        # Check if user is logged in
-        if 'user_id' in request.session and request.session['user_id'] is not None:
-            authenticated = True
+        if request.user.is_authenticated():
+            return redirect('/dashboard')
+        user_form = self.form_class()
+        return render(request, 'registration/register.html', {'form': user_form})
 
-        return render(request, 'index.html', {'authenticated': authenticated})
-
-
-def AuthenticateView(request):
-    if not request.is_ajax():
-        return HttpResponseNotAllowed(['POST'])
-
-    id_token = request.META['HTTP_AUTHORIZATION'].split(' ').pop()
-
-    # Verify id token
-    claims = google.oauth2.id_token.verify_firebase_token(id_token, HTTP_REQUEST)
-    if not claims:
-        return HttpResponse(status=401)
-
-    # Set session data
-    request.session['user_id'] = claims['user_id']
-    request.session['email'] = claims['email']
-    return HttpResponse(status=200)
-
-
-def LogoutView(request):
-    if not request.is_ajax() or not request.method == 'POST':
-        return HttpResponseNotAllowed(['POST'])
-
-    request.session.clear()
-    return HttpResponse(status=200)
-
+    def post(self, request):
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            # Creating a new user
+            user = user_form.save(commit=False) #don't save to db, we do this after setting the password
+            user.set_password(user.password)
+            user.save()
+            # log the user in and send them to the homepage
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            login(request, user)
+            return HttpResponseRedirect('/dashboard')
+            # TODO: sending a welcome email to the new user needs to be implemented here
+        else:
+            return render(request, self.template_name, {'form': user_form})
 
 '''
 device dashboard page controller
 TODO: users can delete each others devices I think
 '''
+
 def DashboardView(request):
-    authenticated = False
-    if 'user_id' not in request.session:
-        return render(request, 'dashboard.html', {'authenticated': authenticated})
+    # get needed variables set up, and try to make sure only the users devices are shown
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/login/?next=%s' % request.path)
+    current_user = request.user
 
-    if request.session['user_id'] is not None:
-        authenticated = True
+    print(request.POST)
 
-    ref = db.reference('users').child(request.session['user_id']).child('devices')
-
-    # Register a new device
+    # if the user clicked register (and dashboard -- that is register a device)
+    # we set the new id and new name as what was submitted in the form
+    # if there are any alerts (invalid id etc), they will get appened to alert
     if request.POST.get('device_id'):
-        new_device_name = request.POST.get('device_name')
         new_device_id = request.POST.get('device_id')
-        ref.update({
-            new_device_id: {
-                'name': new_device_name
-            }
-        })
-
-    # Delete a device
+        new_device_name = request.POST.get('device_name')
+        Device.objects.register_device(new_device_id, new_device_name, current_user)
+    # if the user clicked delete
+    # we delete the specified device
     elif request.POST.get('delete'):
         device_id = request.POST.get('delete')
-        ref.child(device_id).delete()
+        device = Device.objects.get(device_id=device_id)
+        device.deactivate_device()
 
-    devices = ref.get()
-
-    return render(request, 'dashboard.html', {'authenticated': authenticated, 'devices': devices})
-
-
-def graph(request, device_id):
-    authenticated = False
-    if 'user_id' not in request.session:
-        return render(request, 'graph.html', {'authenticated': authenticated})
-
-    if request.session['user_id'] is not None:
-        authenticated = True
-
-    # Get info associated with requested device
-    device = db.reference('devices').child(device_id)
-    rooms = device.child('rooms').order_by_child('solar').get()
-
-    return render(request, 'graph.html', {'authenticated': authenticated, 'rooms': rooms})
-
+    connected_user_devices = Device.objects.filter(user=current_user, is_active=True)
+    return render(request, 'dashboard.html', {'devices': connected_user_devices})
 
 def TimerView(request):
     # get needed variables set up, and try to make sure only the users devices are shown
-    if 'user_id' not in request.session:
+    if not request.user.is_authenticated():
         return HttpResponseRedirect('/login/?next=%s' % request.path)
+    current_user = request.user
 
-    authenticated = False
-    if request.session['user_id'] is not None:
-        authenticated = True
+    connected_user_devices = Device.objects.filter(user=current_user, is_active=True)
 
-    return render(request, 'timer.html', {'authenticated': authenticated})
-
+    return render(request, 'timer.html', {'devices': connected_user_devices})
 
 def DevicesView(request):
     # get needed variables set up, and try to make sure only the users devices are shown
-    # current_user = request.user
-    #
-    # # if the user clicked the editable field and submitted an edit
-    # # changes the edited field to the new submission
-    # if request.POST.get('name') == "modify":
-    #     device_id = request.POST.get('pk')
-    #     device = Device.objects.get(device_id=device_id)
-    #     new_name = request.POST.get('value')
-    #     device.name = new_name
-    #     device.save()
-    # # if the user clicked register
-    # # we set the new id and new name as what was submitted in the form
-    # # if there are any alerts (invalid id etc), they will get appened to alert
-    # elif request.POST.get('register'):
-    #     new_device_id = request.POST.get('device_id')
-    #     new_device_name = request.POST.get('device_name')
-    #     Device.objects.register_device(new_device_id, new_device_name, current_user)
-    #
-    # # if the user clicked delete
-    # # fetch request, process data package
-    # elif request.POST.get('delete'):
-    #     device_id = request.POST.get('delete')
-    #     device = Device.objects.get(device_id=device_id)
-    #     device.deactivate_device()
-    #
-    # user_devices = Device.objects.filter(user=current_user, is_active=True)
-    return render(request, 'devices.html')
+    current_user = request.user
 
+    # if the user clicked the editable field and submitted an edit
+    # changes the edited field to the new submission
+    if request.POST.get('name') == "modify":
+        device_id = request.POST.get('pk')
+        device = Device.objects.get(device_id=device_id)
+        new_name = request.POST.get('value')
+        device.name = new_name
+        device.save()
+    # if the user clicked register
+    # we set the new id and new name as what was submitted in the form
+    # if there are any alerts (invalid id etc), they will get appened to alert
+    elif request.POST.get('register'):
+        new_device_id = request.POST.get('device_id')
+        new_device_name = request.POST.get('device_name')
+        Device.objects.register_device(new_device_id, new_device_name, current_user)
+
+    # if the user clicked delete
+    # fetch request, process data package
+    elif request.POST.get('delete'):
+        device_id = request.POST.get('delete')
+        device = Device.objects.get(device_id=device_id)
+        device.deactivate_device()
+
+    user_devices = Device.objects.filter(user=current_user, is_active=True)
+    return render(request, 'devices.html', {'devices': user_devices})
+
+def graph(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/login/?next=%s' % request.path)
+    current_user = request.user
+
+    connected_user_devices = Device.objects.filter(user=current_user, is_active=True)
+
+    return render(request, 'graph.html', {'devices': connected_user_devices})
+
+
+### WEB2PY API ###
+
+def get_data():
+    today = datetime.utcnow()
+    if request.get_vars.start is not None:
+        start = request.vars.start
+        start = start.split(',')
+        start = datetime(int(start[0]),int(start[1]),int(start[2]))
+        print(start)
+    else:
+        raise HTTP(400, "No room specified")
+    if request.get_vars.end is not None:
+        end = request.vars.end
+        end = end.split(',')
+        end = datetime(int(end[0]), int(end[1]), int(end[2]))
+        print(end)
+    else:
+        end = None
+    if request.get_vars.room is not None:
+        room = request.vars.room
+    else:
+        raise HTTP(400,"No room specified")
+    if request.get_vars.device is not None:
+        device = request.vars.device
+    else:
+        raise HTTP(400, "No device specified")
+
+    # text = 'connection success'
+    data = []
+    time = []
+    if request.get_vars.end is None: #get one day from device usage
+        rows = get_oneday(room, device, start)
+        for i, r in enumerate(rows):
+            data.append(r.use_time)
+            time.append(r.on_off)
+    else: # get a time period from daily usage
+        rows = get_period(room, device, start, end)
+        for i, r in enumerate(rows):
+            data.append(r.total_usage)
+            time.append(r.use_day)
+
+    return response.json(dict(
+        device=device,
+        data=data,
+        time=time,
+    ))
+
+
+def get_room():
+    rows = db(db.user_info.user_id==request.vars.user_id).select()  #user_info
+    rooms = ['Reserve For Home']
+    row = rows[0]
+    rowDict = json.loads(row['rooms'])
+    roomsDict = rowDict['rooms']
+    for r in roomsDict:
+        rooms.append(
+            dict(
+                room=r,
+                icon_path=roomsDict[r]['icon_path'],
+                device=roomsDict[r]['device'],
+                mod_header=roomsDict[r]['mod_header'],
+            )
+        )
+    print(rooms)
+    return response.json(dict(
+        rooms=rooms,
+    ))
+
+
+# @auth.requires_signature()
+def add_room():
+    action_room = db(db.demo_rooms).select().first()
+    action_room.room = ",".join(request.post_vars.rooms)
+    action_room.update_record()
+    print("Insert success")
+    return get_room()
+
+
+def get_oneday(room, dev, day):
+    r = (db.device_usage.room == room)
+    d = (db.device_usage.device == dev)
+    s = (db.device_usage.use_time >= day)
+    e = (db.device_usage.use_time < day+timedelta(days=1))
+    rows = db(r & d & s & e).select(orderby=db.device_usage.use_time)
+    return rows
+
+def get_period(room, dev, start, end):
+    r = (db.daily_usage.room == room)
+    d = (db.daily_usage.device == dev)
+    s = (db.daily_usage.use_day >= start)
+    e = (db.daily_usage.use_day <= end)
+    rows = db(r & d & s & e).select(orderby=db.daily_usage.use_day)
+    return rows
