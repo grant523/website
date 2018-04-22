57801f6aebf2c5cfa43a5740a84a7a695d588903
Kevin Jung <rubic22@gmail.com>
2018-04-22 14:28:11 -0700

    added start of gathering a list of devices dependent on selected room

--- seadssite/static/js/dashboard.js
+++ seadssite/static/js/dashboard.js
@@ -3,7 +3,28 @@ $(function () {
         delimiters: ['${', '}'],
         el: '#vue-div',
         data: {
-            'devices': user_devices
+            'devices': user_devices,
+            'selectedRoom': 'ROOM1'
+        },
+        methods: {
+        	// isCorrectRoom takes in the selectedRoom from the managebar, 
+        	// and the name of the room we're comparing it to
+        	// it returns whether they are equal or not.
+        	// this function is necessary due to vue not having an equal operator 
+        	isCorrectRoom: function(roomName) {
+        		if(this.selectedRoom == roomName) {
+        			console.log(""+ this.selectedRoom + "|" + roomName);
+        			// to modify the data:
+        			// this.selectedRoom = 'ROOM2';
+        			return true;
+        		} else {
+        			return false;
+        		}
+        	}
+
+        	// setSelectedRoom: function(roomName)
+
+
         }
     })
 })
